d8fd1c620b58b5a21e7e7e282f7c84c426eedfc2
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-05-27 07:17:11 +0100

    new commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
+// document.querySelector('.app').innerHTML = `
 //   <div>
 //     <a href="https://vitejs.dev" target="_blank">
 //       <img src="${viteLogo}" class="logo" alt="Vite logo" />
